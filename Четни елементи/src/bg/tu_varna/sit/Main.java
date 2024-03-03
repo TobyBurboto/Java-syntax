1b8cc147253502fc7723c3cfd050e567566aa1ae
TobyBurboto <126807003+TobyBurboto@users.noreply.github.com>
2024-03-03 20:30:47 +0200

    Update Main.java

--- Четни елементи/src/bg/tu_varna/sit/Main.java
+++ Четни елементи/src/bg/tu_varna/sit/Main.java
@@ -5,7 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        float[] numbers = {2.64f,3.54f,0.71f,2.56f,4.39f,6.18f,3.21f,3.85f,7.09f,4.78f};
+        float[] numbers = {1.23f, 2.45f, 4.67f, 3.89f, 6.01f, 9.34f, 8.56f, 5.78f, 7.90f, 10.12f};
         float[] biggestEven = { 0f, 0f, 0f };
         int maxEven = 0;
 
